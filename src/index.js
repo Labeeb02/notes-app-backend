c32a6fc52deed994775057e84afae1fb935d1848
Labeeb02 <ahsan.labeeb@gmail.com>
2022-06-01 18:32:43 +0530

    support for netlify site 2.0

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'https://stirring-alpaca-d4425c.netlify.app/', 
+    origin:'https://stirring-alpaca-d4425c.netlify.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
